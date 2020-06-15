ec5327a9d368302c11bb548266f67d3bf7baa4fd
egprida <egprida@EGPRIDA10.indra.es>
2020-06-15 17:50:37 +0200

    pruebas autoconfigure

---
+++ src/main/java/com/mkyong/config/FooClass.java
@@ -0,0 +1,5 @@
+package com.mkyong.config;
+
+public class FooClass {
+
+}
--- src/main/java/com/mkyong/config/SpringWebConfig.java
+++ src/main/java/com/mkyong/config/SpringWebConfig.java
@@ -1,8 +1,10 @@
 package com.mkyong.config;
  
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -12,6 +14,7 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @Configuration
 @ComponentScan({ "com.mkyong.web" })
+@Import(FooClass.class)
 public class SpringWebConfig extends WebMvcConfigurerAdapter {
  
 	@Override
@@ -28,4 +31,19 @@ public class SpringWebConfig extends WebMvcConfigurerAdapter {
 		return viewResolver;
 	}
  
+	@ConditionalOnMissingBean(InternalResourceViewResolver.class)
+	@Bean
+	public FooClass fooclass(){
+		System.out.println("Prueba no tiene que pasar");
+		return new FooClass();
+		
+	}
+ 
+	@ConditionalOnMissingBean(BazzClass.class)
+	@Bean
+	public VarClass varclass(){
+		System.out.println("Prueba ok");
+		return new VarClass();
+		
+	}
 }
